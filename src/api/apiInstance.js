521fb6d34cf9235bf56ec68a60bb71af3d050f5c
Ben Maitland <benwaltonmaitland@gmail.com>
2021-11-05 23:10:58 -0400

    Login stores auth info in SecureStore

--- src/api/apiInstance.js
+++ src/api/apiInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3001/api/';
+const BASE_URL = 'http://192.168.0.187:3001/api/';
 
 export const Api = () => {
 
--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -5,11 +5,12 @@ import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
 //import { Context } from '../context/AuthContext';
 import Api from '../api/apiInstance';
+import * as SecureStore from 'expo-secure-store';
 
-function SigninScreen() {
+function SigninScreen({navigator}) {
   //const { state, signin, clearErrorMessage } = useContext(Context);
   
-  const handleLogin = (email, password) => {
+  const handleLogin = async (email, password) => {
     console.log("email: ", email);
     console.log("password: ", password);
     const body = {
@@ -26,10 +27,25 @@ function SigninScreen() {
     })
     .then((response) => {
       console.log("response.data: ", response.data);
+      saveAuthInfo(response.data.accessToken, response.data.refreshToken, response.data.user);
     })
     .catch((e) => {
+      console.log("e.response: ", e.response);
       console.log("e.message: ", e.message);
     });
+  }
+
+  const saveAuthInfo = async (accessToken, refreshToken, user) => {
+    await SecureStore.setItemAsync("accessToken", accessToken);
+    await SecureStore.setItemAsync("refreshToken", refreshToken);
+    await SecureStore.setItemAsync("firstName", user.firstName);
+    await SecureStore.setItemAsync("lastName", user.last_name);
+    await SecureStore.setItemAsync("email", user.email);
+    console.log("await SecureStore.getItemAsync('accessToken'): ", await SecureStore.getItemAsync("accessToken"))
+    console.log("await SecureStore.getItemAsync('refreshToken'): ", await SecureStore.getItemAsync("refreshToken"))
+    console.log("await SecureStore.getItemAsync('firstName'): ", await SecureStore.getItemAsync("firstName"))
+    console.log("await SecureStore.getItemAsync('lastName'): ", await SecureStore.getItemAsync("lastName"))
+    console.log("await SecureStore.getItemAsync('email'): ", await SecureStore.getItemAsync("email"))
   }
 
   return (
